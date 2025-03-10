6173f982edb0529fed651d6e3223372aa44e0ca2
sarthak.whenever@gmail.com <sarthak.whenever@gmail.com>
2025-03-11 03:04:10 +0545

    ci added

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Skip certain warnings
+        if (warning.code === 'THIS_IS_UNDEFINED') return
+        if (warning.code === 'CIRCULAR_DEPENDENCY') return
+        
+        // Use default for everything else
+        warn(warning)
+      }
+    }
+  },
+  logLevel: 'error', // Only show errors, not warnings
 })
